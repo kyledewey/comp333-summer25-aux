3c55ed432de18f2c2c02f334109141c5b23c97ad
Kyle Dewey <kyle.dewey@csun.edu>
2025-06-23 14:33:28 -0700

    .

--- javascript/objects.js
+++ javascript/objects.js
@@ -18,9 +18,42 @@ function getArea(rec) {
     return rec.width * rec.height;
 }
 
+// let rectanglePrototype = {
+//     'getArea': function() {
+//         return this.width * this.height;
+//     }
+// };
+
+// function Rectangle(w, h) {
+//     // this = {}
+//     this.width = w;
+//     this.height = h;
+//     this.__proto__ = rectanglePrototype;
+//     // return this;
+// }
+
+//      Shape
+//      /  \
+// Square  Rectangle
+function Shape() {}
+
+function Square(side) {
+    this.side = side;
+}
+Square.prototype = new Shape();
+Square.prototype.getArea = function() {
+    return this.side * this.side;
+};
+
 function Rectangle(w, h) {
-    // this = {}
+    // this = {};
+    // this.__proto__ = Rectangle.prototype;
     this.width = w;
     this.height = h;
     // return this;
 }
+Rectangle.prototype = new Shape();
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+}
+
